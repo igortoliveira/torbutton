a35a746ca206706f4c5742d70bc90b649c1bfdf0
Igor Oliveira <igt0@torproject.org>
2018-02-28 17:42:33 -0300

    bug 25013: Update XPCOM calls after changes in the interfaces

--- src/chrome/content/torbutton.js
+++ src/chrome/content/torbutton.js
@@ -7,7 +7,6 @@
 // TODO: Double-check there are no strange exploits to defeat:
 //       http://kb.mozillazine.org/Links_to_local_pages_don%27t_work
 
-let { LoadContextInfo } = Cu.import('resource://gre/modules/LoadContextInfo.jsm', {});
 let { Services } = Cu.import("resource://gre/modules/Services.jsm", {});
 let { showDialog } = Cu.import("resource://torbutton/modules/utils.js", {});
 let { getLocale, unescapeTorString } = Cu.import("resource://torbutton/modules/utils.js", {});
@@ -733,8 +732,7 @@ function torbutton_do_async_versioncheck() {
   torbutton_log(3, "Checking version with socks port: "
           +m_tb_prefs.getIntPref("network.proxy.socks_port"));
   try {
-    var req = Components.classes["@mozilla.org/xmlextras/xmlhttprequest;1"]
-                            .createInstance(Components.interfaces.nsIXMLHttpRequest);
+    var req = new XMLHttpRequest();
     //var req = new XMLHttpRequest(); Blocked by content policy
     var url = m_tb_prefs.getCharPref("extensions.torbutton.versioncheck_url");
     req.open('GET', url, true);
@@ -1130,6 +1128,9 @@ function torbutton_do_new_identity() {
   torbutton_log(3, "New Identity: Clearing Offline Cache");
 
   try {
+    const LoadContextInfo = Cc["@mozilla.org/load-context-info-factory;1"]
+      .getService(Ci.nsILoadContextInfoFactory);
+
     for (let contextInfo of [LoadContextInfo.default, LoadContextInfo.private]) {
       let appCacheStorage = Services.cache2.appCacheStorage(contextInfo, null);
       // The following call (asyncEvictStorage) is actually synchronous, either
@@ -2023,7 +2024,7 @@ let stopLanguagePromptObserver;
 function torbutton_new_window(event)
 {
     torbutton_log(3, "New window");
-    var browser = getBrowser();
+    var browser = window.gBrowser;
 
     if(!browser) {
       torbutton_log(5, "No browser for new window.");
